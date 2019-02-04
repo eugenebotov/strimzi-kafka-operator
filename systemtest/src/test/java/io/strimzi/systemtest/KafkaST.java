c2ef2f4c53c11920f261ebce7ab88039280c4edf
serrss <sergey.romanov.kh@gmail.com>
2019-02-04 18:32:48 +0200

    1. Created St to check EO deletion

--- systemtest/src/test/java/io/strimzi/systemtest/KafkaST.java
+++ systemtest/src/test/java/io/strimzi/systemtest/KafkaST.java
@@ -8,6 +8,7 @@ import io.fabric8.kubernetes.api.model.Event;
 import io.fabric8.kubernetes.api.model.batch.Job;
 import io.strimzi.api.kafka.Crds;
 import io.strimzi.api.kafka.model.CertSecretSource;
+import io.strimzi.api.kafka.model.Kafka;
 import io.strimzi.api.kafka.model.KafkaClusterSpec;
 import io.strimzi.api.kafka.model.KafkaListenerAuthenticationScramSha512;
 import io.strimzi.api.kafka.model.KafkaListenerAuthenticationTls;
@@ -19,10 +20,14 @@ import io.strimzi.api.kafka.model.PasswordSecretSource;
 import io.strimzi.api.kafka.model.ZookeeperClusterSpec;
 import io.strimzi.test.timemeasuring.Operation;
 import io.strimzi.test.timemeasuring.TimeMeasuringSystem;
-import io.strimzi.test.annotations.OpenShiftOnly;
-import io.strimzi.test.extensions.StrimziExtension;
+import io.strimzi.systemtest.timemeasuring.Operation;
+import io.strimzi.systemtest.timemeasuring.TimeMeasuringSystem;
 import io.strimzi.systemtest.utils.StUtils;
 import io.strimzi.test.TestUtils;
+import io.strimzi.test.annotations.ClusterOperator;
+import io.strimzi.test.annotations.Namespace;
+import io.strimzi.test.annotations.OpenShiftOnly;
+import io.strimzi.test.extensions.StrimziExtension;
 import io.strimzi.test.k8s.Oc;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
@@ -56,10 +61,14 @@ import static io.strimzi.systemtest.k8s.Events.SuccessfulDelete;
 import static io.strimzi.systemtest.k8s.Events.Unhealthy;
 import static io.strimzi.systemtest.matchers.Matchers.hasAllOfReasons;
 import static io.strimzi.systemtest.matchers.Matchers.hasNoneOfReasons;
+import static io.strimzi.test.TestUtils.fromYamlString;
+import static io.strimzi.test.TestUtils.map;
+import static io.strimzi.test.TestUtils.waitFor;
 import static io.strimzi.test.extensions.StrimziExtension.ACCEPTANCE;
-import static io.strimzi.test.extensions.StrimziExtension.FLAKY;
 import static io.strimzi.test.extensions.StrimziExtension.CCI_FLAKY;
+import static io.strimzi.test.extensions.StrimziExtension.FLAKY;
 import static io.strimzi.test.extensions.StrimziExtension.REGRESSION;
+import static io.strimzi.test.extensions.StrimziExtension.TOPIC_CM;
 import static io.strimzi.test.TestUtils.fromYamlString;
 import static io.strimzi.test.TestUtils.map;
 import static io.strimzi.test.TestUtils.waitFor;
@@ -181,6 +190,19 @@ class KafkaST extends AbstractST {
         //Test that CO doesn't have any exceptions in log
         TimeMeasuringSystem.stopOperation(operationID);
         assertNoCoErrorsLogged(TimeMeasuringSystem.getDurationInSecconds(testClass, testName, operationID));
+    }
+
+    @Test
+    @Tag(REGRESSION)
+    void testEODeletion () {
+        // Deploy kafka cluster with EO
+        Kafka kafka = resources().kafkaEphemeral(CLUSTER_NAME, 3).done();
+        // Remove EO from Kafka DTO
+        kafka.getSpec().setEntityOperator(null);
+        // Replace Kafka configuration with removed EO
+        resources.kafka(kafka).done();
+        // Wait when EO(UO + TO) will be removed
+        kubeClient.waitForResourceDeletion("deployment", entityOperatorDeploymentName(CLUSTER_NAME));
     }
 
     @Test
