9dbdb18029a873a3c626763f5c941064ba0538a8
eugenebotov <botov96@mail.ru>
2019-02-21 17:34:23 +0300

    Updated zookeeper nodes configs

--- docker-images/zookeeper/scripts/zookeeper_config_generator.sh
+++ docker-images/zookeeper/scripts/zookeeper_config_generator.sh
@@ -4,7 +4,8 @@
 cat <<EOF
 # the directory where the snapshot is stored.
 dataDir=${ZOOKEEPER_DATA_DIR}
-clientPort=$(expr 10 \* 2181 + $ZOOKEEPER_ID - 1)
+#clientPort=$(expr 10 \* 2181 + $ZOOKEEPER_ID - 1)
+clientPort=21810
 
 # Provided configuration
 ${ZOOKEEPER_CONFIGURATION}
@@ -12,9 +13,9 @@ ${ZOOKEEPER_CONFIGURATION}
 EOF
 
 NODE=1
-FOLLOWER_PORT=$(expr 10 \* 2888)
-ELECTION_PORT=$(expr 10 \* 3888)
+#FOLLOWER_PORT=$(expr 10 \* 2888)
+#ELECTION_PORT=$(expr 10 \* 3888)
 while [ $NODE -le $ZOOKEEPER_NODE_COUNT ]; do
-  echo "server.${NODE}=my-cluster-zookeeper:$(expr $FOLLOWER_PORT + $NODE - 1):$(expr $ELECTION_PORT + $NODE - 1)"
+  echo "server.${NODE}=my-cluster-zookeeper-${NODE}.my-cluster-zookeeper:2888:3888"
   let NODE=NODE+1
 done
