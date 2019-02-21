685c900a04ed905d0f8d54ea27205ec4c6e005ce
eugenebotov <botov96@mail.ru>
2019-02-21 14:15:18 +0300

    DNS instead of IP for follower & election

--- docker-images/zookeeper/scripts/zookeeper_config_generator.sh
+++ docker-images/zookeeper/scripts/zookeeper_config_generator.sh
@@ -15,6 +15,6 @@ NODE=1
 FOLLOWER_PORT=$(expr 10 \* 2888)
 ELECTION_PORT=$(expr 10 \* 3888)
 while [ $NODE -le $ZOOKEEPER_NODE_COUNT ]; do
-  echo "server.${NODE}=0.0.0.0:$(expr $FOLLOWER_PORT + $NODE - 1):$(expr $ELECTION_PORT + $NODE - 1)"
+  echo "server.${NODE}=my-cluster-zookeeper:$(expr $FOLLOWER_PORT + $NODE - 1):$(expr $ELECTION_PORT + $NODE - 1)"
   let NODE=NODE+1
 done
