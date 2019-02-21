8a9ef7c85b7c04bece09646269dc8d12b94c0ad2
eugenebotov <botov96@mail.ru>
2019-02-21 18:48:39 +0300

    Changed client port

--- docker-images/zookeeper/scripts/zookeeper_healthcheck.sh
+++ docker-images/zookeeper/scripts/zookeeper_healthcheck.sh
@@ -1,7 +1,8 @@
 #!/usr/bin/env bash
 
 ZOOKEEPER_ID=$(hostname | awk -F'-' '{print $NF+1}')
-CLIENT_PORT=$(expr 21810 + $ZOOKEEPER_ID - 1)
+# CLIENT_PORT=$(expr 21810 + $ZOOKEEPER_ID - 1)
+CLIENT_PORT=21810
 
 OK=$(echo ruok | nc 127.0.0.1 $CLIENT_PORT)
 if [ "$OK" == "imok" ]; then
